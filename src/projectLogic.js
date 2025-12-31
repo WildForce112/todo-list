f70848ede6e968eb968f3dcbca198ea2433508fa
WildForce112 <81035327+WildForce112@users.noreply.github.com>
2026-01-01 01:32:33 +0700

    fix: default project not working when there is none in localstorage

--- src/projectLogic.js
+++ src/projectLogic.js
@@ -50,11 +50,11 @@ var projects = [];
 const day = new Date();
 
 if(!getStorage()){
-  const Personal = createNewProject("Personal");
+  const Personal = createNewProject("Personal", []);
   Personal.addTodoToProject(createNewTodo("Buy groceries", day.setDate(day.getDate())));
   Personal.addTodoToProject(createNewTodo("Workout 30 minutes", day.setHours(day.getHours() + 27)));
   projects.push(Personal);
-  const School = createNewProject("School");
+  const School = createNewProject("School", []);
   School.addTodoToProject(createNewTodo("Finish math homework", day.setHours(day.getHours() + 57)));
   School.addTodoToProject(createNewTodo("Prepare presentation", day.setDate(day.getDate() + 6)));
   projects.push(School);
